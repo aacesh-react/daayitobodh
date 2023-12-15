91b95765e2121dea52477c7055186aef22e92911
aacesh <52647571+aacesh@users.noreply.github.com>
2023-12-15 15:55:27 +0545

    Update endpoints.js

--- src/app/endpoints.js
+++ src/app/endpoints.js
@@ -1,6 +1,6 @@
 const endpoints = {
-  // apiBaseURL: "https://api.pizzastation.com.np/api/v1",
-  apiBaseURL: "https://api.v1.dayitwabodh.com/api/v1",
+  apiBaseURL: "https://api.pizzastation.com.np/api/v1",
+  // apiBaseURL: "https://api.v1.dayitwabodh.com/api/v1",
   // apiBaseURL: "http://localhost:5000/api/v1",
   roles: "roles",
   userRoles: "user/roles",
